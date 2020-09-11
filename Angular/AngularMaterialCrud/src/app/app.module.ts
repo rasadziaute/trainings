147bfae08ca5f830820e119e4b627599086a7d19
VILNIUS\Rasa.Baniulyte <rasa.baniulyte@euromonitor.com>
2020-09-11 16:46:12 +0300

    first commit

---
+++ Angular/AngularMaterialCrud/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeService } from './employee.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EmployeeChartDataComponent } from './employee-chart-data/employee-chart-data.component';
+import { ChartsModule } from 'ng2-charts';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    EmployeeListComponent,
+    AddEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeeChartDataComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AngularMaterialModule,
+    ReactiveFormsModule,
+    ChartsModule,
+    FormsModule
+
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
